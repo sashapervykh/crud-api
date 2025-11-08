6deef94ae3772a2f67945c92e58bef75a3fe34a1
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-11-08 17:40:46 +0300

    feat: handle get request to one user

---
+++ src/handlers/handleOneUserRequesr/handleOneUserRequest.ts
@@ -0,0 +1,51 @@
+import { type IncomingMessage, type ServerResponse } from 'node:http';
+import { METHODS } from '../../constants/methods.ts';
+import { database } from '../../database/database.ts';
+import { validate } from 'uuid';
+
+export function handleOneUserRequest(
+  req: IncomingMessage,
+  res: ServerResponse<IncomingMessage> & {
+    req: IncomingMessage;
+  },
+) {
+  const urlSegments = req.url?.split('/').filter(Boolean);
+  if (!urlSegments) {
+    res.writeHead(500);
+    res.end(
+      'Unexpected error happened. Server did not receive information about requested resource.',
+    );
+    return;
+  }
+
+  if (!validate(urlSegments[2])) {
+    res.writeHead(400);
+    res.end('Received user id is not valid uuid');
+    return;
+  }
+
+  const requestedUser = database.find((elem) => elem.id === urlSegments[2]);
+
+  if (!requestedUser) {
+    res.writeHead(404);
+    res.end('Requested user does not exist');
+    return;
+  }
+
+  if (urlSegments?.length > 3) {
+    res.writeHead(404);
+    res.end('Requested resource does not exist! Please check your link...');
+    return;
+  }
+
+  switch (req.method) {
+    case METHODS.GET: {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(requestedUser));
+      break;
+    }
+    default: {
+      res.end(`Method ${req.method} is not supported for path /api/users`);
+    }
+  }
+}
--- src/index.mts
+++ src/index.mts
@@ -2,6 +2,7 @@ import http from 'node:http';
 import dotenv from 'dotenv';
 import { BASE_LINK } from './constants/path.ts';
 import { handleAllUsersRequest } from './handlers/handleAllUsersRequest/handleAllUsersRequest.ts';
+import { handleOneUserRequest } from './handlers/handleOneUserRequesr/handleOneUserRequest.ts';
 
 dotenv.config({ path: './.env' });
 
@@ -9,11 +10,12 @@ const PORT = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
   switch (true) {
-    case req.url === BASE_LINK: {
+    case req.url === BASE_LINK || req.url === `${BASE_LINK}/`: {
       handleAllUsersRequest(req, res);
       break;
     }
-    case req.url?.startsWith(BASE_LINK): {
+    case req.url?.startsWith(`${BASE_LINK}/`): {
+      handleOneUserRequest(req, res);
       break;
     }
     default: {
